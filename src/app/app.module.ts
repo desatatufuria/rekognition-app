8cdb2ed02893bb501e530b85eff53b8a64bd2edb
furia <jcarlos@desatatufuria.com>
2024-06-03 11:49:51 +0200

    fix jpelirrojo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageUploadComponent } from './image-upload/image-upload.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { ImageUploadComponent } from './image-upload/image-upload.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-image-upload',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./image-upload.component.css']
 })
 export class ImageUploadComponent {
+  selectedFile: File | null = null;
+  imageUrl: string | ArrayBuffer | null = null;
+  jsonResponse: any = null;
 
+  constructor(private http: HttpClient) { }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+
+    if (this.selectedFile) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imageUrl = e.target.result;
+      };
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    console.log(this.selectedFile);
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('file', this.selectedFile, this.selectedFile.name);
+
+      this.http.post('https://localhost:7130/api/Image/upload', formData)
+        .subscribe(response => {
+          this.jsonResponse = response;
+          console.log(this.jsonResponse);
+        }, error => {
+          console.error('Error:', error);
+        });
+    }
+  }
 }
