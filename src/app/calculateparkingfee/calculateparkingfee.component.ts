3d5ab9687b3317e9198cc0866273f00919b44f50
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-06-10 18:40:46 +0200

    parking fee details fixed

--- src/app/calculateparkingfee/calculateparkingfee.component.ts
+++ src/app/calculateparkingfee/calculateparkingfee.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ParkingService } from '../parking.service';
 import { DatePipe } from '@angular/common';
+import { QrDataService } from '../qr-data.service';
 
 @Component({
   selector: 'app-calculateparkingfee',
@@ -8,11 +9,21 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./calculateparkingfee.component.css'],
   providers: [DatePipe]
 })
-export class CalculateparkingfeeComponent {
+export class CalculateparkingfeeComponent implements OnInit {
   licensePlate: string = ''; // Initialize licensePlate property
   data: any;
 
-  constructor(private parkingService: ParkingService, private datePipe: DatePipe) { }
+  constructor(private parkingService: ParkingService, private datePipe: DatePipe, private qrDataService: QrDataService) { }
+
+  ngOnInit() {
+    // Suscribirse a los cambios en los datos del QR
+    this.qrDataService.qrData$.subscribe(data => {
+      if (data) {
+        this.licensePlate = data;
+        this.fetchData();
+      }
+    });
+  }
 
   fetchData() {
     this.parkingService.fetchData(this.licensePlate).subscribe(
--- src/app/qr-scanner/qr-scanner.component.ts
+++ src/app/qr-scanner/qr-scanner.component.ts
@@ -1,25 +1,31 @@
-import { Component, ElementRef, OnInit, ViewChild, NgZone } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, NgZone, OnDestroy } from '@angular/core';
 import QrScanner from 'qr-scanner';
+import { QrDataService } from '../qr-data.service';
 
 @Component({
   selector: 'app-qr-scanner',
   templateUrl: './qr-scanner.component.html',
   styleUrls: ['./qr-scanner.component.css']
 })
-export class QrScannerComponent implements OnInit {
-  @ViewChild('video', { static: true })
-  video!: ElementRef<HTMLVideoElement>;
+export class QrScannerComponent implements OnInit, OnDestroy {
+  @ViewChild('video', { static: true }) video!: ElementRef<HTMLVideoElement>;
+  @Output() scanResult = new EventEmitter<string>(); // Emitir evento cuando se detecte un QR
   result: string | null = null;
+  qrScanner!: QrScanner;
 
-  constructor(private ngZone: NgZone) { }
+  constructor(private ngZone: NgZone, private qrDataService: QrDataService) { }
 
   ngOnInit(): void {
     this.initializeScanner();
   }
 
+  ngOnDestroy(): void {
+    this.qrScanner.stop();
+  }
+
   initializeScanner(): void {
     const videoElement = this.video.nativeElement;
-    const qrScanner = new QrScanner(
+    this.qrScanner = new QrScanner(
       videoElement,
       result => this.ngZone.run(() => this.setResult(result)),
       {
@@ -28,11 +34,12 @@ export class QrScannerComponent implements OnInit {
       }
     );
 
-    qrScanner.start();
+    this.qrScanner.start();
   }
 
   setResult(result: any): void {
     this.result = result.data;
     console.log('QR Code detected:', this.result);
+    this.qrDataService.setQrData(this.result!); // Enviar el resultado al servicio
   }
 }
