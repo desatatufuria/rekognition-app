ae6b92034d4a81fedcac9fae6990832cc6dd3c1e
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-06-10 17:02:28 +0200

    parking fee details

--- src/app/calculateparkingfee/calculateparkingfee.component.ts
+++ src/app/calculateparkingfee/calculateparkingfee.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { ParkingService } from '../parking.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-calculateparkingfee',
   templateUrl: './calculateparkingfee.component.html',
-  styleUrls: ['./calculateparkingfee.component.css']
+  styleUrls: ['./calculateparkingfee.component.css'],
+  providers: [DatePipe]
 })
 export class CalculateparkingfeeComponent {
   licensePlate: string = ''; // Initialize licensePlate property
   data: any;
 
-  constructor(private parkingService: ParkingService) { }
+  constructor(private parkingService: ParkingService, private datePipe: DatePipe) { }
 
   fetchData() {
     this.parkingService.fetchData(this.licensePlate).subscribe(
@@ -21,5 +23,9 @@ export class CalculateparkingfeeComponent {
         console.error('Error fetching data:', error);
       }
     );
+  }
+
+  formatTime(dateTime: string): string {
+    return this.datePipe.transform(dateTime, 'dd/MM/yyyy HH:mm:ss')!;
   }
 }
