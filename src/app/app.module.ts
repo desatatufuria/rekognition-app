2ec30182277784c6c881b31a66744d5a058cd076
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-06-14 16:50:05 +0200

    delete base64-image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { ImageUploadComponent } from './image-upload/image-upload.component';
 import { HttpClientModule } from '@angular/common/http';
 import { QrcodegeneratorComponent } from './qrcodegenerator/qrcodegenerator.component';
-import { Base64ImageComponent } from './base64-image/base64-image.component';
 import { GarageManagementComponent } from './garage-management/garage-management.component';
 import { LicensedetectorComponent } from './licensedetector/licensedetector.component';
 import { LicenseUploadComponent } from './license-upload/license-upload.component';
@@ -24,7 +23,6 @@ import { ExitComponent } from './exit/exit.component';
     AppComponent,
     ImageUploadComponent,
     QrcodegeneratorComponent,
-    Base64ImageComponent,
     GarageManagementComponent,
     LicensedetectorComponent,
     LicenseUploadComponent,
