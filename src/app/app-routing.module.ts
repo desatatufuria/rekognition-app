6f61e1edc338ff77b7c292daf4509cee599a5721
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-06-14 17:09:56 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { EntryComponent } from './entry/entry.component';
 import { PaymentComponent } from './payment/payment.component';
 import { ExitComponent } from './exit/exit.component';
-import { GarageManagementComponent } from './garage-management/garage-management.component';
 
 const routes: Routes = [
   { path: 'entry', component: EntryComponent },
