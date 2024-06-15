76a06e97c349182c64fe9032c85871015b523bc9
furia <jcarlos@desatatufuria.com>
2024-06-15 12:22:28 +0200

    .

--- src/app/paypal/paypal.component.ts
+++ src/app/paypal/paypal.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, NgZone, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { ParkingService } from '../services/parking.service';
+import { QrDataService } from '../services/qr-data.service';
+import QrScanner from 'qr-scanner';
+import { HttpClient } from '@angular/common/http';
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-paypal',
