3193939ef33fdf546122dd59756fcf95053ca9f6
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-06-16 17:24:18 +0200

    spots style

--- src/app/parking-status/parking-status.component.ts
+++ src/app/parking-status/parking-status.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ParkingSpot } from '../interfaces/parking';
+import { ParkingExit, ParkingSpot } from '../interfaces/parking';
 import { ParkingHttpService } from '../services/parking-http.service';
 
 @Component({
@@ -10,16 +10,29 @@ import { ParkingHttpService } from '../services/parking-http.service';
 export class ParkingStatusComponent implements OnInit {
 
   parkingSpots: ParkingSpot[] = [];
+  vehicles: ParkingExit[] = [];
 
   constructor(private parkingService: ParkingHttpService) { }
 
   ngOnInit(): void {
     this.loadParkingSpots();
+    this.loadVehicles();
   }
 
   loadParkingSpots(): void {
     this.parkingService.getParkingSpots().subscribe(spots => {
       this.parkingSpots = spots;
     });
+  }
+
+  loadVehicles(): void {
+    this.parkingService.getVehicles().subscribe(vehicles => {
+      this.vehicles = vehicles;
+    });
+  }
+
+  getLicensePlate(spotId: number): string | null {
+    const vehicle = this.vehicles.find(v => v.parkingSpotId === spotId && !v.exitTime);
+    return vehicle ? vehicle.licensePlate : null;
   }
 }
--- src/app/services/parking-http.service.ts
+++ src/app/services/parking-http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ParkingSpot } from '../interfaces/parking';
+import { ParkingExit, ParkingSpot } from '../interfaces/parking';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,10 @@ export class ParkingHttpService {
 
   getParkingSpots(): Observable<ParkingSpot[]> {
     return this.http.get<ParkingSpot[]>(`${this.baseUrl}/api/Parking/spots`);
+  }
+
+  getVehicles(): Observable<ParkingExit[]> {
+    return this.http.get<ParkingExit[]>(`${this.baseUrl}/api/parking/vehicles`);
   }
 
   checkAvailableSpots(): Observable<{ availableSpots: number }> {
